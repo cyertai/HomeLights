b807a5169cd4cc6a676131debb9f6c662e9ca837
glenn <cyertai@philen.com>
2021-02-27 16:08:17 -0500

    client side update

--- HomeLightsLAN.py
+++ HomeLightsLAN.py
@@ -2,7 +2,6 @@
 
 from phue import Bridge
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
 import time
 import pdb;
 b = Bridge('192.168.1.142')
@@ -16,20 +15,21 @@ lights = b.lights
 while(1):
     lightsCurrentStatus = -1
     time.sleep(1)
-    response = urlopen('https://www.cyertai.com/lights/status')
-    lightsCurrentStatus = response.read().decode('UTF-8')
-    breakpoint()
+    try:
+        response = urlopen('https://www.cyertai.com/lights/status')
+        lightsCurrentStatus = response.read().decode('UTF-8')
 
-    for light in lights:
-        if (lightsCurrentStatus == "1"):
-            if(light.on == False):
-                light.on = True
-                light.transiiontime = 1
-                light.brightness = 254
-        if (lightsCurrentStatus == "0"):
-            if(light.on == True):
-                light.on = False
-        if (lightsCurrentStatus == "-1"):
-            pass
+        for light in lights:
+            if (lightsCurrentStatus == "1"):
+                if(light.on == False):
+                    light.on = True
+                    light.transiiontime = 1
+                    light.brightness = 254
+            if (lightsCurrentStatus == "0"):
+                if(light.on == True):
+                    light.on = False
+            if (lightsCurrentStatus == "-1"):
+                pass
 
-
+    except:
+        pass
